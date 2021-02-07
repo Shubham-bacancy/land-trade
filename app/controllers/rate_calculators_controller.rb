eaead7905ea3998133c47342d300a4a5ff8b54f1
Shubham Rajput <shubham.rajput@bacancy.com>
2021-02-07 20:24:58 +0530

    Calculate received principal

--- app/controllers/rate_calculators_controller.rb
+++ app/controllers/rate_calculators_controller.rb
@@ -2,15 +2,6 @@ class RateCalculatorsController < ApplicationController
   include FinanceCalculation
 
   def index
-    # @principal_balance = params["unpaid_principal_balance"].to_f
-    # @wac = params["wac"].to_f
-    # @servicing_fee = params["servicing_fee"].to_f
-    # @remaining_amortization_term = params["remaining_amortization_term"].to_f
-    # @remaining_term_to_maturity = params["remaining_term_to_maturity"].to_i
-    # @loss_assumption = params["loss_assumption"].to_f
-    # @prepayment_assumption = params["prepayment_assumption"].to_f
-    # @price_paid = params["price_paid"].to_f
-
     # unless params["principal_balance"] && params["duration"] && params["wac"] 
     #   flash[:errors] = "Please enter all required fields.."
     #   redirect_to rate_calculators_new_path and return true
@@ -19,6 +10,12 @@ class RateCalculatorsController < ApplicationController
       @wac = params["wac"].to_f
       @duration = params["remaining_amortization_term"].to_f #params["duration"].to_i 
       @servicing_fee = params["servicing_fee"].to_f
+      @remaining_amortization_term = params["remaining_amortization_term"].to_f
+      @remaining_term_to_maturity = params["remaining_term_to_maturity"].to_i
+      @loss_assumption = params["loss_assumption"].to_f
+      @prepayment_assumption = params["prepayment_assumption"].to_f
+      @price_paid = params["price_paid"].to_f
+
       @scheduled_interest = calculate_scheduled_interest(@principal_balance, @wac).round
       @scheduled_payment = calculate_scheduled_payment
       @scheduled_principal = calculate_scheduled_principal
@@ -32,7 +29,7 @@ class RateCalculatorsController < ApplicationController
   private
 
   def calculate_scheduled_interest(principal_balance, wac)
-    (principal_balance * wac)/(12 * 100)
+    (principal_balance * wac)/(12 * 100) rescue 0
   end
 
   def calculate_scheduled_payment
@@ -40,7 +37,7 @@ class RateCalculatorsController < ApplicationController
   end
 
   def calculate_scheduled_principal
-    (calculate_scheduled_payment - calculate_scheduled_interest(@principal_balance, @wac)).round rescue 0
+    (calculate_scheduled_payment - calculate_scheduled_interest(@principal_balance, @wac)) rescue 0
   end
 
   def ending_performing_balance
@@ -48,11 +45,26 @@ class RateCalculatorsController < ApplicationController
   end
 
   def calculate_servicing_fee(perf_principal)
-    (perf_principal*@servicing_fee)/(12*100) 
+    (perf_principal*@servicing_fee)/(12*100) rescue 0 
+  end
+
+  def calculate_default(perf_principal)
+    (perf_principal*@loss_assumption)/(12*100) rescue 0
   end
 
   def calculate_received_interest(scheduled_interest, servicing_fee)
     scheduled_interest - servicing_fee
+  end
+
+  def calculate_received_principal(prepayment, scheduled_principal)
+    (prepayment + scheduled_principal) rescue 0
+  end
+
+  def calculate_prepayment(begining_perf_principal, scheduled_principal)
+    minus_amt = begining_perf_principal - scheduled_principal
+    monthly = 0.08 # 1/12
+    prepay_assumption = (1-@prepayment_assumption/100)
+    ((100*(minus_amt)*(1-(prepay_assumption)**(monthly))+0.5)/100) rescue 0
   end
 
   def set_arrays_of_calculations
@@ -62,7 +74,10 @@ class RateCalculatorsController < ApplicationController
     @end_perf_balances = []
     @servicing_fees = []
     @received_interests = []
-    
+    @prepayments = []
+    @defaults = []
+    @principal_received = []
+
     for i in(0..@duration)
       if i==0
         @payments << @principal_balance # begining principle
@@ -71,13 +86,19 @@ class RateCalculatorsController < ApplicationController
         @servicing_fees << calculate_servicing_fee(@principal_balance) #servicing_fees
         @end_perf_balances << @ending_perf_balance # Ending perf balance
         @received_interests << calculate_received_interest(@interests[i], @servicing_fees[i]) #received_interest
+        @prepayments << calculate_prepayment(@principal_balance, @scheduled_principal) # prepayment
+        @defaults << calculate_default(@principal_balance) #calculate default
+        @principal_received << calculate_received_principal(@prepayments[0], @scheduled_principal) # principal received
       else
         @payments << @end_perf_balances[i-1]
         @interests << calculate_scheduled_interest(@payments[i], @wac)
         @principals << (@scheduled_payment - @interests[i])
         @servicing_fees << calculate_servicing_fee(@payments[i])
+        @defaults << calculate_default(@payments[i])
         @end_perf_balances << (@payments[i] - @principals[i])
         @received_interests << calculate_received_interest(@interests[i], @servicing_fees[i])
+        @prepayments << calculate_prepayment(@payments[i], @principals[i])
+        @principal_received << calculate_received_principal(@prepayments[i], @principals[i])
       end 
     end
   end
